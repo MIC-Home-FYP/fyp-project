ba2aca38679e757adeb705213d529dc9605f8092
Green-Tea-123 <junboonsoo@gmail.com>
2025-02-15 16:50:09 +0800

    Update navigator with label text styling

--- lib/PageNavigator.dart
+++ lib/PageNavigator.dart
@@ -3,6 +3,7 @@ import 'package:fyp_project/HomePage.dart';
 import 'package:fyp_project/VitalsPage.dart';
 import 'package:fyp_project/PillsPage.dart';
 import 'package:fyp_project/ChatPage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PageNavigator extends StatefulWidget {
   static String patientName = "Candice Ng";
@@ -17,21 +18,28 @@ class _PageNavigatorState extends State<PageNavigator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.blueGrey,
-        selectedIndex: currentPageIndex,
-        backgroundColor: Color(0xFF779FCA),
-        destinations: const <Widget> [
-          NavigationDestination(icon: Icon(Icons.home_outlined, color: Colors.white,), label: 'Home'), 
-          NavigationDestination(icon: Icon(Icons.monitor_heart_outlined, color: Colors.white,), label: 'Vitals'), 
-          NavigationDestination(icon: Icon(Icons.medication_outlined, color: Colors.white,), label: 'Pills'), 
-          NavigationDestination(icon: Icon(Icons.chat_bubble_outline_rounded, color: Colors.white,), label: 'Chat'), 
-        ],
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          backgroundColor: Color(0xFF779FCA),
+          indicatorColor: Colors.blueGrey,
+          labelTextStyle: WidgetStatePropertyAll(GoogleFonts.roboto(
+            color: Colors.white,
+          )),
+        ),
+        child: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget> [
+            NavigationDestination(icon: Icon(Icons.home_outlined, color: Colors.white,), label: 'Home'), 
+            NavigationDestination(icon: Icon(Icons.monitor_heart_outlined, color: Colors.white,), label: 'Vitals'), 
+            NavigationDestination(icon: Icon(Icons.medication_outlined, color: Colors.white,), label: 'Pills'), 
+            NavigationDestination(icon: Icon(Icons.chat_bubble_outline_rounded, color: Colors.white,), label: 'Chat'), 
+          ],
+        ),
       ),
 
       body: 
